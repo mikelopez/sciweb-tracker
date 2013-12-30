8be0e89b06d8a095f1052c877c73e7b24bbc02b9
Mike Lopez <dev@scidentify.info>
2013-12-30 02:21:33 -0500

    basic linktracking model

--- sciweb_tracker/models.py
+++ sciweb_tracker/models.py
@@ -5,3 +5,12 @@ Visit track is just tracking a page visit.
 Object track is tracking a particular object or link that was clicked.
 """
 TRACK_TYPES = ['visit', 'object']
+
+class LinkTracking(models.Model):
+    """Track a particular link and redirect (optional)"""
+    name = models.CharField(max_length=30)
+    item_type = models.CharField(max_length=20, blank=True, null=True)
+    item_id = models.IntegerField(blank=True, null=True)
+    item_ctype = models.CharField(max_length=30, blank=True, null=True)
+
+
