8ebef0d30cf826924e37b5d1549f8901930033a3
Mike Lopez <dev@scidentify.info>
2014-01-01 03:53:29 -0500

    some model changes

--- sciweb_tracker/models.py
+++ sciweb_tracker/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
 """
-Visit track is just tracking a page visit.
-Object track is tracking a particular object or link that was clicked.
+Track and keep logs of what people do
 """
+
 TRACK_TYPES = ['visit', 'object']
 
-class LinkTracking(models.Model):
+class Tracking(models.Model):
     """Track a particular link and redirect (optional)"""
     name = models.CharField(max_length=30)
     item_type = models.CharField(max_length=20, blank=True, null=True)
     item_id = models.IntegerField(blank=True, null=True)
     item_ctype = models.CharField(max_length=30, blank=True, null=True)
     counter = models.IntegerField(default=0, blank=True, null=True)
+    ipaddress = models.CharField(max_length=20, blank=True, null=True)
     admin = models.NullBooleanField(default=False)
+    date_tracked = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    redirect_to = models.TextField(blank=True, null=True)
 
 
